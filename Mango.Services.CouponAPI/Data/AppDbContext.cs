87405cafee65a9577fbec474e0f6873245bade8c
Konstantin Kuznetsov <denttera@gmail.com>
2024-02-24 21:52:54 +0300

    database context introduced

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    #region Properties
+
+    private DbSet<Coupon> Coupons { get; set; }
+
+    #endregion
+
+    #region Construction
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    #endregion
+}
--- Mango.Services.CouponAPI/Models/Coupon.cs
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -1,13 +1,18 @@
-﻿namespace Mango.Services.CouponAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models;
 
 public class Coupon
 {
     #region Properties
 
+    [Required]
     public string CouponCode { get; set; }
 
+    [Key]
     public int CouponId { get; set; }
 
+    [Required]
     public double DiscountAmount { get; set; }
 
     public int MinAmount { get; set; }
