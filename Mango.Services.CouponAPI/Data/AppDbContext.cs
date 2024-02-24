5d72c64d3eaca15103db21f6500fedb49e5a0de5
Konstantin Kuznetsov <denttera@gmail.com>
2024-02-24 22:12:13 +0300

    migrations added

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -18,4 +18,29 @@ public class AppDbContext : DbContext
     }
 
     #endregion
+
+    #region Private Methods
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+                                              {
+                                                  CouponId = 1,
+                                                  CouponCode = "10OFF",
+                                                  DiscountAmount = 10,
+                                                  MinAmount = 20
+                                              });
+
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+                                              {
+                                                  CouponId = 2,
+                                                  CouponCode = "20OFF",
+                                                  DiscountAmount = 20,
+                                                  MinAmount = 40
+                                              });
+    }
+
+    #endregion
 }
