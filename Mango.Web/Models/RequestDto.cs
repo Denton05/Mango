fd797f893c02f457287025247ae95bc178c15b67
Konstantin Kuznetsov <denttera@gmail.com>
2024-02-25 14:42:58 +0300

    web project models added

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Web.Utility;
+
+namespace Mango.Web.Models;
+
+using static SD;
+
+public class RequestDto
+{
+    #region Properties
+
+    public string AccessToken { get; set; }
+
+    public ApiType ApiType { get; set; } = ApiType.GET;
+
+    public object Data { get; set; }
+
+    public string Url { get; set; }
+
+    #endregion
+}
---
+++ Mango.Web/Models/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Mango.Web.Models;
+
+public class ResponseDto
+{
+    #region Properties
+
+    public bool IsSuccess { get; set; } = true;
+
+    public string Message { get; set; } = string.Empty;
+
+    public object? Result { get; set; }
+
+    #endregion
+}
