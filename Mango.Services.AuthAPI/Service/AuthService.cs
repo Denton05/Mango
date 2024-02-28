e306f1180dd4d17980e9051ff30d80e472614b24
Konstantin Kuznetsov <denttera@gmail.com>
2024-02-28 12:12:29 +0300

    AuthService introduced

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,43 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service;
+
+public class AuthService : IAuthService
+{
+    #region Fields
+
+    private readonly AppDbContext _context;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    #endregion
+
+    #region Construction
+
+    public AuthService(AppDbContext context, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+    {
+        _context = context;
+        _roleManager = roleManager;
+        _userManager = userManager;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
