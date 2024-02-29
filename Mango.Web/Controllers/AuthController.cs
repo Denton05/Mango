53b1c69505f35bcba0e5ddfdd327524df5a4d50f
Konstantin Kuznetsov <denttera@gmail.com>
2024-02-29 11:33:44 +0300

    Auth items added to View

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        #region Fields
+
+        private readonly IAuthService _authService;
+
+        #endregion
+
+        #region Construction
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var loginRequestDto = new LoginRequestDto();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
