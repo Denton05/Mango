e72901096f0ce42cf255263466c7a3257c8e5a81
Konstantin Kuznetsov <denttera@gmail.com>
2024-03-01 17:05:12 +0300

    home controller updated

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,14 +1,44 @@
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
+        #region Fields
+
+        private readonly IProductService _productService;
+
+        #endregion
+
+        #region Construction
+
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        #endregion
+
         #region Public Methods
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto>? list = new();
+            var response = await _productService.GetAllProductsAsync();
+
+            if(response is { IsSuccess: true })
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(list);
         }
 
         #endregion
