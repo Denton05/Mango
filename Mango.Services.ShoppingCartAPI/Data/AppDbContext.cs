02465181281ad81bc58edbd6d40d7db5ad706025
Konstantin Kuznetsov <denttera@gmail.com>
2024-03-01 18:37:03 +0300

    chopping cart service configurated

---
+++ Mango.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    #region Properties
+
+    public DbSet<CartDetails> CartDetails { get; set; }
+
+    public DbSet<CartHeader> CartHeaders { get; set; }
+
+    #endregion
+
+    #region Construction
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    #endregion
+}
---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI;
+
+public class MappingConfig
+{
+    #region Public Methods
+
+    public static MapperConfiguration RegisterMaps()
+    {
+        var mappingConfig = new MapperConfiguration(config =>
+                                                    {
+                                                        config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                                                        config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                                                    });
+
+        return mappingConfig;
+    }
+
+    #endregion
+}
